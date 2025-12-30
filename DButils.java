96c7716d4dbb982d07b57bc7771db69ad1634aba
devopstraining <soumyasoubhagya.kumar@gmail.com>
2025-12-30 14:06:19 +0000

    New branch check

--- DButils.java
+++ DButils.java
@@ -1,3 +1,4 @@
 hello this file contains the DB releated functions.
 commit without config but now config to check.
 Check files status which are committed but not pushed to remote repository.
+New branch created and editing to check merge.
