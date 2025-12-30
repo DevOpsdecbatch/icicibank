becc97f8768709bdc75c05dae1e0a3fdf32da73c
devopstraining <soumyasoubhagya.kumar@gmail.com>
2025-12-30 18:55:09 +0000

    new branch uat check

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,4 @@
 This file contains the deployment script.
+New branch addition,commit and code push.
+Create pull request and merge successfully.
+Then delete the branch from git and from working space.
